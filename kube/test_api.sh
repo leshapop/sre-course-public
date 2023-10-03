7eada872064068b6ae4a85d8577f9af2e61b0f6c
lpop <leshapop@gmail.com>
2023-10-03 18:47:43 +0300

    Update test_api.sh

--- kube/test_api.sh
+++ kube/test_api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X "GET" "http://91.185.85.213/WeatherForecast" -H "Host:weather.student71.local"
+curl -X "GET" "http://xxx.xxx.xxx.xxx/WeatherForecast" -H "Host:weather.student71.local"
