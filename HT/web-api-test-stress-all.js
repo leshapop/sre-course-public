2e157835ca2c3f8541b8610ea119c6221245e24a
lpop <leshapop@gmail.com>
2023-12-01 08:36:29 +0300

    add new stress test for all endpoints

--- HT/web-api-test-stress-all.js
+++ HT/web-api-test-stress-all.js
@@ -8,52 +8,47 @@ const API_BASE_URL = 'http://91.185.85.213'
 const API_GET_ENDPOINT1 = 'WeatherForecast'
 const API_GET_ENDPOINT2 = 'Cities/1'
 const API_HOST = 'weather.student71.local'
-const API_POST_ENDPOINT = 'Cities'
-const API_POST_ID = 3
-const VUS_GET = 2
-const TARGET_GET = 2
-const VUS_POST = 2
-const TARGET_POST = 2
-const DURATION = '1s' // SLO Load time
-const DURATION_POST = '1s' // SLO Load time
+const API_ENDPOINTS = ['Cities', 'Forecast'];
+const VUS = [1, 1]
+const TARGET = [50, 50]
+const DURATION = ['1m', '1m'] // SLO Load time
 const SLO_ERRORS = 'rate<=0.01'
 const SLO_REQ_DURATION = 'p(95)<=500'
 const SLO_ERRORS_ABORT = true
 const SLO_REQ_DURATION_ABORT = true
 
 //Fake data for POST REQ
-let Cities = Array('Казань', 'Тверь', 'Белгород', 'Рязань', 'Таганрог', 'Новочеркасск');
+let Cityid = [2, 3, 4];
+let Cities = ['TEST_Казань', 'TEST_Тверь', 'TEST_Белгород', 'TEST_Рязань', 'TEST_Таганрог', 'TEST_Новочеркасск', 'TEST_Ростов-на-Дону', 'TEST_Москва'];
+let Summary = ['TEST_Облачно', 'TEST_Солнечно', 'TEST_Дождь', 'TEST_Ясно', 'TEST_Снег'];
 
 //options
 export const options = {
   scenarios: {
-    get: {
+    cities: {
       executor: 'ramping-arrival-rate',
-      exec: 'breakpoint_get',
-      startTime: '5s',
-      gracefulStop: '5s',
+      exec: 'stress_cities',
+      startTime: '1s',
+      gracefulStop: '1s',
 
       StartRate: 1,
       timeUnit: '1s',
-      preAllocatedVUs: `${VUS_GET}`,
+      preAllocatedVUs: `${VUS[0]}`,
       stages: [
-
-        { target: `${TARGET_GET}`, duration: `2m` },
-        { target: `${TARGET_GET}`, duration: `${DURATION}` },
+        { target: `${TARGET[0]}`, duration: `${DURATION[0]}` },
       ],
     },
-    post: {
+    forecast: {
       executor: 'ramping-arrival-rate',
-      exec: 'breakpoint_post',
-      startTime: '5s',
-      gracefulStop: '5s',
+      exec: 'stress_forecast',
+      startTime: '1s',
+      gracefulStop: '1s',
 
       StartRate: 1,
       timeUnit: '1s',
-      preAllocatedVUs: `${VUS_POST}`,
+      preAllocatedVUs: `${VUS[1]}`,
       stages: [
-        { target: `${TARGET_POST}`, duration: `2m` },
-        { target: `${TARGET_POST}`, duration: `${DURATION_POST}` },
+        { target: `${TARGET[1]}`, duration: `${DURATION[1]}` },
       ],
     },
   },
@@ -90,30 +85,87 @@ const params = {
 //Cities endpoints requests
 export function stress_cities() {
 
-  const res = http.get(`${API_BASE_URL}/Cities/`, params);
-  console.log(res.json());
-  sleep(1);
+//  console.log("******");
+
+  const res = http.get(`${API_BASE_URL}/${API_ENDPOINTS[0]}/`, params);
+//  let API_POST_ID = randomItem(res.json("#.id"));
+  let API_POST_ID = randomItem(Cityid);
+
+  const res_get_cityid = http.get(`${API_BASE_URL}/${API_ENDPOINTS[0]}/${API_POST_ID}`, params);
+  let API_PUT_CITY_NAME = res_get_cityid.json("name");
+//  console.log("DEFAULT VALUE = "+API_PUT_CITY_NAME);
+
+  let data = { id: `${API_POST_ID}`, name: randomItem(Cities) };
+  const put = http.put(`${API_BASE_URL}/${API_ENDPOINTS[0]}/${API_POST_ID}`, JSON.stringify(data), params);
+
+  const res_get_cityid2 = http.get(`${API_BASE_URL}/${API_ENDPOINTS[0]}/${API_POST_ID}`, params);
+  let API_PUT_CITY_NAME2 = res_get_cityid2.json("name");
+//  console.log("NEW VALUE = "+API_PUT_CITY_NAME2);
+
+  let data_back = { id: `${API_POST_ID}`, name: `${API_PUT_CITY_NAME}` };
+  const put_back = http.put(`${API_BASE_URL}/${API_ENDPOINTS[0]}/${API_POST_ID}`, JSON.stringify(data_back), params);
+
+  const res_get_cityid3 = http.get(`${API_BASE_URL}/${API_ENDPOINTS[0]}/${API_POST_ID}`, params);
+  let API_PUT_CITY_NAME3 = res_get_cityid3.json("name");
+//  console.log("RETURN VALUE = "+API_PUT_CITY_NAME3);
+
   check(res, {
-    "GET status is 200": (r) => r.status === 200
+    "C_res status is 200": (r) => r.status === 200
   });
-  const res = http.get(`${API_BASE_URL}/Cities/`, params);
-  sleep(1);
-  check(res, {
-    "GET status is 200": (r) => r.status === 200
+  check(res_get_cityid, {
+    "C_res_get_cityid status is 200": (r) => r.status === 200
+  });
+  check(put, {
+    "C_put status is 200": (r) => r.status === 200
+  });
+  check(put_back, {
+    "C_put_back status is 200": (r) => r.status === 200
   });
 
+  sleep(1);
 
 }
-//Forecast endpoints requests
+
+//Cities endpoints requests
 export function stress_forecast() {
 
-//Post data
-  const data = { id: `${API_POST_ID}`, name: randomItem(Cities) };
-  const put = http.put(`${API_BASE_URL}/${API_POST_ENDPOINT}/${API_POST_ID}`, JSON.stringify(data), params);
-  sleep(1);
+//  console.log("******");
+
+  let res = http.get(`${API_BASE_URL}/${API_ENDPOINTS[1]}/`, params);
+//  let API_POST_ID = randomItem(res.json("#.id"));
+  let API_POST_ID = randomItem(Cityid);
+
+  let res_get_forecastid = http.get(`${API_BASE_URL}/${API_ENDPOINTS[1]}/${API_POST_ID}`, params);
+  let API_PUT_FORECAST_SUMMARY = res_get_forecastid.json();
+//  console.log("DEFAULT VALUE = "+API_PUT_FORECAST_SUMMARY.summary);
+
+  let data = { id: `${API_POST_ID}`, cityId: API_PUT_FORECAST_SUMMARY.cityId, datetime: 222222222, temperature: 111111, summary: randomItem(Summary) };
+  let put = http.put(`${API_BASE_URL}/${API_ENDPOINTS[1]}/${API_POST_ID}`, JSON.stringify(data), params);
+
+  let res_get_forecastid2 = http.get(`${API_BASE_URL}/${API_ENDPOINTS[1]}/${API_POST_ID}`, params);
+  let API_PUT_FORECAST_SUMMARY2 = res_get_forecastid2.json("summary");
+//  console.log("NEW VALUE = "+API_PUT_FORECAST_SUMMARY2);
+
+  let data_back = { id: `${API_POST_ID}`, cityId: API_PUT_FORECAST_SUMMARY.cityId, datetime: API_PUT_FORECAST_SUMMARY.datetime, temperature: API_PUT_FORECAST_SUMMARY.temperature, summary: API_PUT_FORECAST_SUMMARY.summary };
+  let put_back = http.put(`${API_BASE_URL}/${API_ENDPOINTS[1]}/${API_POST_ID}`, JSON.stringify(data_back), params);
+
+  let res_get_forecastid3 = http.get(`${API_BASE_URL}/${API_ENDPOINTS[1]}/${API_POST_ID}`, params);
+  let API_PUT_FORECAST_SUMMARY3 = res_get_forecastid3.json("summary");
+//  console.log("RETURN VALUE = "+API_PUT_FORECAST_SUMMARY3);
+
+  check(res, {
+    "FC_res status is 200": (r) => r.status === 200
+  });
+  check(res_get_forecastid, {
+    "FC_res_get_forecastid status is 200": (r) => r.status === 200
+  });
   check(put, {
-    "PUT status is 200": (p) => p.status === 200
+    "FC_PUT status is 200": (r) => r.status === 200
+  });
+  check(put_back, {
+    "FC_put_back status is 200": (r) => r.status === 200
   });
 
+  sleep(1);
 
 }
